dce1849b434bfd2bfe828232d84dc07c3f5c7120
Digdarshan <lambahidman701@gmail.com>
2024-06-16 19:44:00 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './output.css';
+import CLayout from "./components/CLayout";
+
+const App = () => {
+  return (
+    <div>
+      <CLayout />
+    </div>
+  );
+};
+
+export default App;
