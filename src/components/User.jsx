a9fe67e7830e47d13e244216ce05594f12cf502b
Digdarshan <lambahidman701@gmail.com>
2024-06-19 17:27:31 +0545

    Managed usersystem

---
+++ src/components/User.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Btnicon from "./Btnicon";
+import { useRef } from "react";
+
+const User = () => {
+  const videostat = useRef({
+    0: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#e8eaed"
+      >
+        <path d="M880-260 720-420v67l-80-80v-287H353l-80-80h367q33 0 56.5 23.5T720-720v180l160-160v440ZM822-26 26-822l56-56L878-82l-56 56ZM498-575ZM382-464ZM160-800l80 80h-80v480h480v-80l80 80q0 33-23.5 56.5T640-160H160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800Z" />
+      </svg>
+    ),
+    1: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#e8eaed"
+      >
+        <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h480q33 0 56.5 23.5T720-720v180l160-160v440L720-420v180q0 33-23.5 56.5T640-160H160Zm0-80h480v-480H160v480Zm0 0v-480 480Z" />
+      </svg>
+    ),
+  });
+  const audiostat = useRef({
+    0: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#e8eaed"
+      >
+        <path d="m710-362-58-58q14-23 21-48t7-52h80q0 44-13 83.5T710-362ZM480-594Zm112 112-72-72v-206q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v126l-80-80v-46q0-50 35-85t85-35q50 0 85 35t35 85v240q0 11-2.5 20t-5.5 18ZM440-120v-123q-104-14-172-93t-68-184h80q0 83 57.5 141.5T480-320q34 0 64.5-10.5T600-360l57 57q-29 23-63.5 39T520-243v123h-80Zm352 64L56-792l56-56 736 736-56 56Z" />
+      </svg>
+    ),
+    1: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#e8eaed"
+      >
+        <path d="M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z" />
+      </svg>
+    ),
+  });
+  return (
+    <div
+      id="me"
+      className=" user text-center relative text-white font-bold user  bg-no-repeat bg-center w-[200px] h-[200px]  bg-[url('https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png')] bg-contain"
+    >
+      <div className="userinfo bg-[rgba(0,0,0,0.5)] text-center">
+        <span className="username">Digdarshan Ghimire</span>
+      </div>
+      <div className="deviceinfo absolute bottom-1 left-1 flex justify-center gap-4">
+        <Btnicon btntype="mic" stat={audiostat} />
+        <Btnicon btntype="video" stat={videostat} />
+      </div>
+    </div>
+  );
+};
+
+export default User;
